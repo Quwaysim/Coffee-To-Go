f9c6a4112801187e026d126eeb3b02fed9226791
Qaasim Muhammad Akorede <qaasim405@gmail.com>
2021-05-21 11:13:57 +0100

    project init - corrected

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
+import 'package:coffee_to_go/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() =>
-  runApp(MyApp());
+void main() => runApp(CoffeeToGo());
 
-class MyApp extends StatelessWidget {
-
+class CoffeeToGo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp
+    return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-
+      initialRoute: HomeScreen.id,
+      routes: {
+        HomeScreen.id: (context) => HomeScreen(),
+      },
     );
   }
 }
